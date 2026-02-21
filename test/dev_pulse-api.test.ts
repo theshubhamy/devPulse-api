183381baf49c55e9d9a610ddead02a3a844ded13
Shubham Kumar <shubham.constructiontechnect@gmail.com>
2026-02-22 02:49:14 +0530

    Initial commit

---
+++ test/dev_pulse-api.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib/core';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as DevPulseApi from '../lib/dev_pulse-api-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/dev_pulse-api-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new DevPulseApi.DevPulseApiStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
